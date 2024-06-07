e4929f6482f318c01f588fd00b42db5ded58954e
BISWORANJAN ROUT <138195813+bisworanjan2004@users.noreply.github.com>
2024-06-07 10:57:33 +0530

    React series

---
+++ react-hooks/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+// import Click from "./components/useeffect/Click";
+// import Fetch from "./components/useeffect/Fetch";
+// import Form from "./components/useref/Form";
+// import Show from "./components/usestate/showtext/Show";
+// import Counter from "./components/usestate/counter/Counter";
+
+function App() {
+  return (
+    <div>
+      <h1>React Hooks</h1>
+      {/* <Counter /> */}
+      {/* <Show /> */}
+      {/* <Form /> */}
+      {/* <Click />
+      <Fetch /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-hooks/src/components/useeffect/Click.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+// rafce snippets
+import React, { useEffect, useState } from "react";
+
+const Click = () => {
+  // when the app renders and re-renders what you want to perform
+  // onMount, stateChange, unMount
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You have clicked ${count} times`;
+
+    return () => {
+      // cleanup function
+      document.title = "React Hooks";
+    };
+  }, [count]);
+
+  function countClick() {
+    setCount((prev) => prev + 1);
+  }
+  console.log(count);
+  // function exit(){
+  //   window.close();
+  // }
+
+  return (
+    <div>
+      <button onClick={countClick}>Click Me</button>
+      {/* <button onClick={exit}>Exit</button> */}
+    </div>
+  );
+};
+
+export default Click;
